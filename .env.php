02daae76f1f8476e41e142758cf29350d82ff374
Luciano Sousa <80105305+lucianosousaferreira@users.noreply.github.com>
2025-06-12 20:56:23 -0300

    Add files via upload

---
+++ .env.php
@@ -0,0 +1,8 @@
+// .env.php (NÃO subir no GitHub)
+<?php
+putenv("DB_HOST=trolley.proxy.rlwy.net");
+putenv("DB_PORT=59498");
+putenv("DB_USER=root");
+putenv("DB_PASSWORD=TSqwfoZpKFxwBPqMsfuElImWOxAIflSB");
+putenv("DB_NAME=oficina");
+?>
