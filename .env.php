ccb3ae2c6f89cc347189cc48e4cafd2ebfa7b620
Luciano Sousa <80105305+lucianosousaferreira@users.noreply.github.com>
2025-06-13 13:22:17 -0300

    Update .env.php

--- .env.php
+++ .env.php
@@ -1,8 +1,1 @@
-// .env.php (NÃO subir no GitHub)
-<?php
-putenv("DB_HOST=trolley.proxy.rlwy.net");
-putenv("DB_PORT=59498");
-putenv("DB_USER=root");
-putenv("DB_PASSWORD=TSqwfoZpKFxwBPqMsfuElImWOxAIflSB");
-putenv("DB_NAME=oficina");
-?>
+
