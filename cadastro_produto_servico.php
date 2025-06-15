22e942ebefd589b56f7fdab3ca5642854c48f772
Luciano Sousa <80105305+lucianosousaferreira@users.noreply.github.com>
2025-06-15 20:40:38 -0300

    Update cadastro_produto_servico.php

--- cadastro_produto_servico.php
+++ cadastro_produto_servico.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require 'vendor/autoload.php'; // Cloudinary via Composer
 include_once 'conexao.php';
 include_once 'verifica_login.php';
