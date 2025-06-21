2dda35110ad7f44dc0f4faccdea10b8ed8eef2e4
Luciano Sousa <80105305+lucianosousaferreira@users.noreply.github.com>
2025-06-20 22:53:20 -0300

    Update verifica_login.php

--- verifica_login.php
+++ verifica_login.php
@@ -1,7 +1,10 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-if (!isset($_SESSION['usuario'])) {
-    header('Location: login.php');
+if (!isset($_SESSION['usuario_id'])) {
+    header("Location: login.php");
     exit();
 }
+?>
